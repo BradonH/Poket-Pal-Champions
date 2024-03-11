eb602468dc612cbbb23e034a19dc697658b17bdf
BradonH <Bradon.hopper@students.fhu.edu>
2024-03-11 18:20:32 -0500

    began creation

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ import abilities
 import item
 import pokemon
 import player
+
+#create pokemon objects
+charmander = pokemon.Poke("Charmander", "Fire", 45, 53, 45)
+
+squirtle = pokemon.Poke("Squirtle", "Water", 50, 48, 65)
+
+bulbasaur = pokemon.Poke("Bulbasuar", "Grass", 50, 50, 50)
+
+pikachu = pokemon.Poke("Pikachu", "Electric", 40, 65, 40)
+
+snorlax = pokemon.Poke("Snorlax", "Normal", 85, 30, 45)
+
+magikarp = pokemon.Poke("Magikarp", "Water", 60, 20, 50)
+
+#create abilities object
+
+#create item object
+
+#create player object
--- pokemon.py
+++ pokemon.py
@@ -1,20 +1,21 @@
-
+import abilities
 
 class Poke():
-    def __init__(self, name, element, hp, atk, defense, P):
+    def __init__(self, name, element, hp, atk, defense):
         self.name = name
         self.element = element
         self.hp = hp
         self.atk = atk
         self.defense = defense
-        self.P = P
+        #self.P = P
         #P stands for Power Points, essentially the stamina a pokemon has
         #it usually knows as PP but to avoid that we'll just call it P
+        #on second thought we wont use P
         self.move_set = []
         self.buff_debuff = []
         #this is where we will keep our buffs or debuffs so we can apply them to the pokemon
 
-    def attacks(self):
-        pass
+    #def attacks(self): idk if this should be here
+        
 
     #def swap_pokemon(self): this is here just incase
